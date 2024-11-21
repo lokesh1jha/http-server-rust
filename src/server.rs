e1d08264f4c06598c4169fefa13d1e70d44703cf
lokesh1jha <lokesh1jha@gmail.com>
2024-11-22 02:35:02 +0530

    http server more sound

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 pub struct Server {
     address: String,
 }
@@ -9,5 +11,20 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.address);
+
+        let listner = TcpListener::bind(self.address).unwrap();
+
+        // infinte loop
+        loop {
+            match listner.accept(){
+                Ok((stream, _)) => {
+                   println!("New connection from {}", stream.peer_addr().unwrap());
+                },
+                Err(e) => {
+                    println!("Failed to connect: {}", e);
+                    continue;
+                }
+            }
+        }
     }
 }
